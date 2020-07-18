cc455a6a340af67969bc7c9ae225dc87b81bca70
wvovaw <vladimir5313375015@gmail.com>
2020-07-19 00:30:56 +0500

    Add xbacklight shortcuts

--- config.h
+++ config.h
@@ -73,7 +73,8 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-b", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-
+static const char *incBackLight[] = {"xbacklight", "+10", NULL};
+static const char *decBackLight[] = {"xbacklight", "-10", NULL};
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
@@ -102,6 +103,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
 	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
+        { MODKEY,                       XK_F8,     spawn,          {.v = decBackLight} },
+        { MODKEY,                       XK_F9,     spawn,          {.v = incBackLight} },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
